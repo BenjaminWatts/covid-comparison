00530cbdf399c0b652f403f7d7e8bc496aa24dcc
Ben Watts <ben@kilowatts.io>
2021-01-26 15:10:41 +0000

    change layout and basic readme

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,5 @@
 import React from "react"
 import { PageProps, graphql } from "gatsby"
-
-// import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { LatestTable } from "../components/latestTable/latestTable"
 import { getLatest, GetLatestData } from "../requests/govUK"
